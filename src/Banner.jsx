b3450c366e03ab0d4666129d864895afbeaccc2d
Saimum-14028 <md.amiralfahim@gmail.com>
2023-12-21 15:48:29 +0600

    Customer Feedback added alhamdulillah

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold text-blue-500">BITE</h1>
-                    <p className="mb-5">Indulge Your Senses in Culinary Excellence at <span>BITE</span>. Where Flavor Meets Elegance</p>
+                    <p className="mb-5">Indulge Your Senses in Culinary Excellence at <span className='text-blue-500 font-semibold'>BITE</span>. Where Flavor Meets Elegance</p>
                     <Link to='/all items'><button className="btn bg-blue-500">Menus</button></Link>
                     </div>
                 </div>
--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -2,8 +2,54 @@ import React from 'react';
 
 const Feedback = () => {
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto my-5'>
+            <div className="mx-auto max-w-3xl text-center">
+                <h3 className="mb-6 text-3xl md:text-4xl lg:text-5xl font-bold">Customer's Feedback</h3>
+            </div>
+
+            <div className="grid gap-12 text-center md:grid-cols-2">
+                {/* <!--First Testimonial--> */}
+                <div className="mb-6 md:mb-0">
+                <div className="mb-6 flex justify-center">
+                    <img
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture"
+                    className="w-24 rounded-full shadow-lg dark:shadow-black/30" />
+                </div>
+                <div className="rating">
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400"/>
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                </div>
+                <p className="my-4 text-xl">
+                    "Lorem ipsum dolor sit amet eos adipisci, consectetur adipisicing
+                    elit sed ut perspiciatis unde omnis."
+                </p>
+                <p className="italic">- Anna Morian</p>
+                </div>
+
+                {/* <!--Second Testimonial--> */}
+                <div className="mb-0">
+                <div className="mb-6 flex justify-center">
+                    <img
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture"
+                    className="w-24 rounded-full shadow-lg dark:shadow-black/30" />
+                </div>
+                <div className="rating">
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400"/>
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                </div>
+                <p className="my-4 text-xl">
+                    "Neque cupiditate assumenda in maiores repudiandae mollitia
+                    architecto elit sed adipiscing elit."
+                </p>
+                <p className="italic">- Teresa May</p>
+                </div>
+            </div>
         </div>
     );
 };
