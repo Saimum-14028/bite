a53942e1a76e750db7b56825bf56b2b528b96e79
Saimum-14028 <md.amiralfahim@gmail.com>
2023-12-21 14:37:35 +0600

    Footer added alhamdulillah

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
                             </NavLink>
                             </li>
                             <li><NavLink
-                                to="/My ordered items"
+                                to="/my ordered items"
                                 className={({ isActive, isPending }) =>
                                     isPending ? "pending" : isActive ? "text-red-500 underline" : ""
                                 }
